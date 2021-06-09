5d45d96d7b2089741958cb6b46018e8e08f0c848
Ann <annie.wang50@yahoo.com>
2021-06-09 18:20:15 -0400

    search bar for movies

--- script.js
+++ script.js
@@ -2,24 +2,26 @@
 
 /* API Info */
 const apiKey = "ec9d43fea05c5d5d0c487254238e6858"
-const apiURL = "https://api.themoviedb.org/3/movie/now_playing?"
+const nowPlayingMovieURL = "https://api.themoviedb.org/3/movie/now_playing?"
 const imageURL = "https://image.tmdb.org/t/p/"
+const searchMovieURL = "https://api.themoviedb.org/3/search/movie?"
 
 /* Other variables */
 var pageNum = 1
 
 /* Query Selectors */
 const movieGrid = document.getElementById('movie-grid')
+const searchMovie = document.querySelector("form")
+
+/* Event Listeners */
+searchMovie.addEventListener("submit", displaySearchResults)
 
 /** Displays the movie grid by adding each movie from a movie list */
 async function updateMovieGrid() {
-  apiURLSearch = apiURL + "api_key=" + apiKey + "&page=" + pageNum
-  const response = await fetch(apiURLSearch)
-  // await fetch(`${apiURL}api_key=${apiKey}&query=${currentMovies}`)
-  console.log(apiURLSearch)
+  nowPlayingMovieSearch = nowPlayingMovieURL + "api_key=" + apiKey + "&page=" + pageNum
+  const response = await fetch(nowPlayingMovieSearch)
   const responseData = await response.json()
 
-  console.log(responseData)
   responseData.results.forEach(element => {
     movieGrid.innerHTML += addMovieToGrid(element)
   });
@@ -36,6 +38,24 @@ function addMovieToGrid(movie) {
       <div class="movie-title">${movie.title}</div>
     </div>
   `
+}
+
+async function displaySearchResults(event) {
+  event.preventDefault()
+  const query = event.target.movie.value
+  searchMovieSearch = searchMovieURL + "api_key=" + apiKey + "&query=" + query
+
+  const response = await fetch(searchMovieSearch)
+  const responseData = await response.json()
+
+  console.log(responseData)
+
+  // clear the movie grid befor displaying results
+  movieGrid.innerHTML = ``
+
+  responseData.results.forEach(element => {
+    movieGrid.innerHTML += addMovieToGrid(element)
+  })
 }
 
 /** Add event listeners on movie containers to make pop-ups */
